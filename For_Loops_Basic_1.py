95b11fbc0a1b82ebf9bb06f4975c033449f5c3a0
vikramkm114a <68656298+vikramkm114a@users.noreply.github.com>
2021-09-23 23:05:15 -0700

    Add files via upload
    basic for loops practice

---
+++ For_Loops_Basic_1.py
@@ -0,0 +1,38 @@
+# for x in range(0, 151):
+#     print(x)
+
+
+# for x in range(5, 1001, 5):
+#     print(x)
+
+
+# for x in range(1, 101, 1):
+#     if x % 5 == 0:
+#         print("Coding")
+#     elif x % 10 == 0:
+#         print("Coding Dojo")
+#     print(x)
+
+
+
+# sum = 0
+
+# for x in range(0, 500001, 1):
+#     if x % 2 != 0:
+#         sum = sum + x
+#     print(sum)
+
+
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+
+
+lownum = 2
+highnum = 9
+mult = 3
+
+for x in range(lownum, highnum):
+    if x % mult == 0:
+        print(x)
