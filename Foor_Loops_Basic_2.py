36e99a73a77c2e367575489ac5436c6d4c427e83
vikramkm114a <68656298+vikramkm114a@users.noreply.github.com>
2021-09-26 23:20:58 -0700

    Add files via upload
    Basic For Loops practice

---
+++ Foor_Loops_Basic_2.py
@@ -0,0 +1,68 @@
+# 1. Biggie Size
+# arr = [-1, 3, 5, -5]
+
+# for i in arr:
+#     if i > 0:
+#         print("Biggie")
+#     else:
+#         print(i)
+# print(i)
+
+
+# 2. Count Positives
+# arr = [1, 6, -4, -2, -7, -2]
+
+# for i in arr:
+#     if i
+
+
+#  3. Sum Total
+# arr = [1, 2, 3, 4]
+
+# def sum(arr):
+#     total = 0
+#     for i in arr:
+#         total += i
+#     return total
+# print(sum(arr))
+
+
+# 4. Average
+# arr = [1, 2, 3, 4]
+
+# def avg(arr):
+#     return sum(arr) / len(arr)
+# print(avg(arr))
+
+
+# # 5. Length
+# arr = [37, 2, 1, -9]
+
+# def length(arr):
+#     return len(arr)
+# print(length(arr))
+
+
+# # 6. Minimum
+# arr = [37, 2, 1, -9]
+
+# def minimum(arr):
+#     return min(arr)
+# print(minimum(arr))
+
+# #  7. Maximum
+# arr = [37, 2, 1, -9]
+
+# def maximum(arr):
+#     return max(arr)
+# print(maximum(arr))
+
+# 8. Ultimate Analysis
+
+
+# 9. Reverse List
+arr = [37, 2, 1, -9]
+
+def reverse(arr):
+    return [ele for ele in reversed(arr)]
+print(reverse(arr))
