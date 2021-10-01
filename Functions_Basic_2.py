903f378cdc4356ab7bc68d9ac351b60e00d7bf01
vikramkm114a <68656298+vikramkm114a@users.noreply.github.com>
2021-09-30 22:22:26 -0700

    Add files via upload
    Basic Functions Practice Python

---
+++ Functions_Basic_2.py
@@ -0,0 +1,40 @@
+# 1. Countdown
+# def countdown(num):
+#     for x in range(num, 0, -1):
+#         print(x)
+
+# countdown(5)
+
+
+# 2. Print and Return
+# arr = [1,3,7,5]
+
+# def print_and_return(arr):
+#     print (arr[0])
+#     return arr[1]
+# print(print_and_return(arr))
+
+
+# 3. First Plus Length
+# def first_plus_length(arr):
+#     return arr[0] + len(arr)
+
+# arr = [1, 2, 4, 6]
+# print (first_plus_length(arr))
+
+
+# 4. Values Greater than Second
+# def greater(arr):
+#       sub_arr = []
+#   if len(arr) == 0:
+#     return False
+#   for x in arr:
+#     if arr[x] > arr[1]:
+#       sub_arr.append(arr[x])
+#   print(sub_arr)
+#   print(len(sub_arr))
+
+
+# 5. This Length, That Value
+
+
